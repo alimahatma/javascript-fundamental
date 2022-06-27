b1e166255143985fe58fb27f5a4909341ce12304
Lalu Mutawalli <laluallistilo@gmail.com>
2022-06-27 17:21:30 +0100

    add latihan REPL & Pengantar Module

---
+++ demo_nodeJS/coba.js
@@ -0,0 +1,7 @@
+// console.log("Hello Gibran");
+
+function cetakNama(nama) {
+    return `Hallo, nama saya adalah ${nama}`;
+}
+
+module.exports = cetakNama;
---
+++ demo_nodeJS/index.js
@@ -0,0 +1,12 @@
+// console.log("Hello WPU");
+
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama('Gbran Raqqila Firaz'));
+
+// console.log(window);
+
+
+const cetakNama = require('./coba');
+ //console.log("Hello World!");
+
+console.log(cetakNama("Gibran Raqqila Firaz"));
